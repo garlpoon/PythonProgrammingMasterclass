9e6fd3a01690dde0ca57eeca04cbb9492b757321
Garland Poon <garlpoon@users.noreply.github.com>
2020-07-04 10:09:37 -0700

    additional escape character

--- src/HelloWorld/escapechar.py
+++ src/HelloWorld/escapechar.py
@@ -6,10 +6,14 @@ print(tabbedString)
 
 print('The pet shop owner said "No, no, \'e\'s uh, .... he\'s resting".')
 print("The pet shop owner said \"no, no, \'e's uh, .... he's resting\".")
-print("""The pet shop owner said "No, no, 'e's uh, .... he's resting".""")
+print("""The pet shop owner said "No, no, \
+'e's uh, .... he's resting".""")
 
 anotherSplitString = """"This string has been \
 split over \
 several
 lines"""
 print(anotherSplitString)
+
+print("C:\\Users\\me\\notes.text")
+print(r"C:\Users\me\notes.text")

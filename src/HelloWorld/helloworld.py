3fbebed90af024bcbc069d341d3c9decc31e09c4
Garland Poon <garlpoon@users.noreply.github.com>
2020-07-04 09:49:05 -0700

    playing with strings

--- src/HelloWorld/helloworld.py
+++ src/HelloWorld/helloworld.py
@@ -1,1 +1,7 @@
-print("Hello World!")
+print('Hello World!')
+print(1 + 2)
+print(6 * 7)
+print()
+print("The end", "or is it?", "maybe", 3)
+
+print("My hovercraft is full of eels")
---
+++ src/HelloWorld/strings.py
@@ -0,0 +1,11 @@
+print("Today is a good day to learn Python")
+print("Python is cool")
+print("Python's string is easy to use")
+print('"Quotes" within strings')
+print("hello" + " world")
+
+greeting = "hi"
+name = input("Please enter your name: ")
+# comment test
+
+print(greeting + " " + name)

6137386a34375d2b18ce2f5555931a82a0f9654b
Garland Poon <garlpoon@users.noreply.github.com>
2020-07-10 19:01:09 -0700

    negative indexes

--- src/HelloWorld/strings2.py
+++ src/HelloWorld/strings2.py
@@ -11,3 +11,11 @@ print(parrot[3])
 print(parrot[6])
 print(parrot[8])
 
+print()
+
+print(parrot[3 - 14])
+print(parrot[4 - 14])
+print(parrot[9 - 14])
+print(parrot[3 - 14])
+print(parrot[6 - 14])
+print(parrot[8 - 14])

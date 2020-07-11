d40e14224af07d8555bc99c502e9ec9a8ca26825
Garland Poon <garlpoon@users.noreply.github.com>
2020-07-10 19:13:11 -0700

    str index slice

--- src/HelloWorld/strings2.py
+++ src/HelloWorld/strings2.py
@@ -2,20 +2,11 @@
 #         012345678901234
 parrot = "Norwegian Blue"
 
-print(parrot)
+print(parrot[0:6]) # Norweg
+print(parrot[3:5]) # we
+print(parrot[0:9]) # we
+print(parrot[:9]) # we
 
-print(parrot[3])
-print(parrot[4])
-print(parrot[9])
-print(parrot[3])
-print(parrot[6])
-print(parrot[8])
-
-print()
-
-print(parrot[3 - 14])
-print(parrot[4 - 14])
-print(parrot[9 - 14])
-print(parrot[3 - 14])
-print(parrot[6 - 14])
-print(parrot[8 - 14])
+print(parrot[10:]) # Blue
+print(parrot[-4:]) # Blue
+print(parrot[:-5]) # Norwegian
